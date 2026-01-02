1a4b42513d6ecdc20ce0b1817b5f701a0254fa9f
millarsfoods <millarsfoods@gmail.com>
2026-01-02 14:33:35 +0000

    fix to location of app

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/crm/nexaloom-crm/',
       server: {
         port: 3006,
         host: '0.0.0.0',
@@ -16,7 +17,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, './'),
         }
       }
     };
